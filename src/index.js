c2337782d5ea24c03ff1d806cdd521c8e9da8fa0
Iris Cheung <hokyinirischeung@gmail.com>
2025-12-03 11:36:43 -0800

    started html and js

---
+++ src/index.js
@@ -0,0 +1,28 @@
+
+const state = {
+    getRealTimeTemperatureButton: null,
+    addOneButton: null,
+    minusOneButton: null,
+    temperatureValue: 0,
+    resetButton: null,
+};
+
+const upArrowClicked = () => {
+    state.temperatureValue += 1;
+    state.tempDisplay.textContent = `${state.temperature-value}`;
+
+
+};
+
+const tempColor = () => {
+    state.
+}
+
+
+const loadControls = () => {
+    state.addOneButton = document.getElementById('increase-temp-btn');
+    state.minusOneButton = document.getElementById('decrease-temp-btn');
+    state.temperatureValue = document.getElementById('temperature-value');
+};
+
+const registerEvents
